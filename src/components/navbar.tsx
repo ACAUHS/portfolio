6e322ba71b503bff791dffd3956878da28ff7b94
Joshua Camacho <shua.camacho@gmail.com>
2023-01-26 03:15:10 -0500

    Add prototype styling

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,8 +1,8 @@
 export const NavBar = () => {
     return (
-        <div className='navbar flex justify-between p-6 bg-'>
-            <div className='text-3xl'>Logo</div>
-            <ul className='flex gap-8 mr-8'>
+        <div className='navbar flex justify-between p-6 bg-blai'>
+            <div className='text-3xl text-yeli'>Logo</div>
+            <ul className='flex gap-8 mr-8 mt-1 text-yeli'>
                 <li>About</li>
                 <li>Skills</li>
                 <li>Work</li>
