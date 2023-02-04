0553cf641d00915ff13891c9790ab94f549e6edb
Joshua-Gabriel Camacho <shua.camacho@gmail.com>
2023-02-04 01:49:19 -0500

    Add stylization to hero section

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,10 +1,13 @@
 export const Hero = () => {
     return (
-        <div className="container flex">
-            <div className="name font-face-bsr">
+        <div className="container flex flex-col">
+            <div className="name font-face-bsr text-6xl self-center">
                 <span className="text-orange">J</span><span className="text-whiter">oshua</span><br />
                 <span className="text-orange">G</span><span className="text-whiter">abriel</span><br />
                 <span className="text-orange">C</span><span className="text-whiter">amacho</span><br />
+            </div>
+            <div>
+                <span className="flex justify-center font-face-bsr text-xl text-whiter">Software Developer</span>
             </div>
         </div>
     )
