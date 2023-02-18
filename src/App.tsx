a82a09b11111ba4e595abaab1f672ad5ccd3db45
Joshua-Gabriel Camacho <shua.camacho@gmail.com>
2023-02-18 01:15:54 -0500

    Add 'About' component to App

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import { NavBar } from './components/NavBar'
 import { Hero } from './components/Hero';
+import { About } from './components/About'
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <div className='landing-section bg-black'>
       <NavBar />
       <Hero />
+      <About />
       </div>
     </div>
   );
---
+++ src/components/About.tsx
@@ -0,0 +1,7 @@
+export const About = () => {
+    return (
+        <div className="bg-whiter flex">
+            <div className="font-face-bsr mx-auto p-10">Joshua-Gabriel Camacho</div>
+        </div>
+    )
+}
