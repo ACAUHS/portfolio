04f17f2fba739fa4934584c27e65d19269b33664
Joshua-Gabriel Camacho <shua.camacho@gmail.com>
2023-02-18 01:04:36 -0500

    Change margin and padding of components

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,13 +1,13 @@
 export const Hero = () => {
     return (
-        <div className="container flex flex-col mx-auto">
+        <div className="container flex flex-col mx-auto py-4">
             <div className="name font-face-bsr text-6xl self-center">
                 <span className="text-orange">J</span><span className="text-whiter">oshua</span><br />
                 <span className="text-orange">G</span><span className="text-whiter">abriel</span><br />
                 <span className="text-orange">C</span><span className="text-whiter">amacho</span><br />
             </div>
             <div>
-                <span className="flex justify-center font-face-bsr text-xl text-whiter">Software Developer</span>
+                <span className="flex justify-center font-face-bsr text-xl text-whiter mx-auto my-2 ">Software Developer</span>
             </div>
         </div>
     )
