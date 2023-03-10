70d9d4deb9f22036130e36b9f28efc64a0577525
Joshua-Gabriel Camacho <shua.camacho@gmail.com>
2023-03-10 17:24:22 -0500

    Add About section information

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,7 +1,14 @@
 export const About = () => {
     return (
-        <div className="bg-whiter flex">
-            <div className="font-face-bsr mx-auto p-10 text-4xl min-h-1/2">About</div>
+        <div className="bg-whiter flex flex-col h-96 mx-auto">
+            <div className="header font-face-bsr mx-auto p-10 text-4xl min-h-1/2">About</div>
+            <div className="main-section flex">
+                <div className="container mx-auto font-face-bsr text-xl w-1/2 pt-10">
+                    My name is Joshua-Gabriel Camacho, and I am a Software Developer based in Toronto/Vancouver.
+                    I mainly work in frontend development, and have an interest in learning the different technologies
+                    available in the industry. I am also passionate about music: most notably hip-hop and jazz. 
+                </div>
+            </div>
         </div>
     )
 }
