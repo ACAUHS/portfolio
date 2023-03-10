38ef6ac72aaf39ca742ac3bf522c69a9b0b27c81
Joshua-Gabriel Camacho <shua.camacho@gmail.com>
2023-03-10 17:43:12 -0500

    Fix padding in Hero section

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -5,7 +5,7 @@ export const Hero = () => {
                 <span className="text-orange">J</span><span className="text-whiter">oshua</span><br />
                 <span className="text-orange">G</span><span className="text-whiter">abriel</span><br />
                 <span className="text-orange">C</span><span className="text-whiter">amacho</span><br />
-                <span className="flex justify-center font-face-bsr text-xl text-whiter mx-auto my-2 ">Software Developer</span>
+                <span className="flex justify-center font-face-bsr text-xl text-whiter mx-auto my-2 pt-4">Software Developer</span>
             </div>
         </div>
     )
