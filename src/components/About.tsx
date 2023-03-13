a3794081770406c323122834110b886fac857407
Joshua-Gabriel Camacho <shua.camacho@gmail.com>
2023-03-13 11:49:18 -0400

    Change About section spacing

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -2,16 +2,16 @@ import img from '../IMG_6636.jpg'
 
 export const About = () => {
     return (
-        <div className="bg-whiter flex flex-col h-screen mx-auto">
-            <div className="header font-face-bsr mx-auto p-10 text-4xl min-h-1/2">About</div>
-            <div className="main-section mx-auto flex justify-center pt-12">
-                <div className="container font-face-bsr text-xl w-1/4 pt-10 pr-24 mr-9">
+        <div className="bg-whiter flex flex-col h-screen justify-center items-center gap-8">
+            <div className="header font-face-bsr text-4xl underline decoration-inherit decoration-2 underline-offset-4">About</div>
+            <div className="main-section flex items-center gap-10 justify-center">
+                <div className="container font-face-bsr text-xl w-1/4">
                     My name is Joshua-Gabriel Camacho, and I am a Software Developer based in Toronto/Vancouver.
                     I mainly work in frontend development, and have an interest in learning the different technologies
                     available in the industry. I am also passionate about music: most notably hip-hop and jazz. 
                 </div>
-                <div className='photo w-1/5 flex-none border-4 border-black'>
-                    <img src={img} />
+                <div className='photo w-1/6 border-black'>
+                    <img className="border-4" src={img} />
                 </div>
             </div>
         </div>
