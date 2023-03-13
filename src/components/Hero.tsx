7fe326fe35b4b7c8e4a376fd13e8c603849db1a9
Joshua-Gabriel Camacho <shua.camacho@gmail.com>
2023-03-13 11:18:08 -0400

    Change spacing for Hero

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,7 +1,9 @@
-export const Hero = () => {
+/** add animation for name and title */
+
+ export const Hero = () => {
     return (
         <div className="container flex flex-col mx-auto h-screen bg-black justify-center">
-            <div className="name font-face-bsr text-6xl self-center place-content-center h-fit">
+            <div className="name font-face-bsr text-6xl self-center place-content-center h-fit mb-48">
                 <span className="text-whiter underline decoration-inherit decoration-2 underline-offset-8">Joshua-Gabriel Camacho</span>
                 <span className="flex justify-center font-face-bsr text-xl text-whiter mx-auto mt-2">Software Developer</span>
             </div>
