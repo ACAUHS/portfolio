f4fa0fc2dbcac1d41f9e33e6581fb29b8e991c95
Joshua-Gabriel Camacho <shua.camacho@gmail.com>
2023-03-13 11:55:09 -0400

    Change About text width

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -4,8 +4,8 @@ export const About = () => {
     return (
         <div className="bg-whiter flex flex-col h-screen justify-center items-center gap-8">
             <div className="header font-face-bsr text-4xl underline decoration-inherit decoration-2 underline-offset-4">About</div>
-            <div className="main-section flex items-center gap-10 justify-center">
-                <div className="container font-face-bsr text-xl w-1/4">
+            <div className="main-section flex gap-10 justify-center">
+                <div className="container font-face-bsr text-xl w-1/5">
                     My name is Joshua-Gabriel Camacho, and I am a Software Developer based in Toronto/Vancouver.
                     I mainly work in frontend development, and have an interest in learning the different technologies
                     available in the industry. I am also passionate about music: most notably hip-hop and jazz. 
