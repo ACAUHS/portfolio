131cde6d12854bf8fad5552b1a63554853381221
Joshua-Gabriel Camacho <shua.camacho@gmail.com>
2023-03-14 19:22:09 -0400

    Add scrolling behaviour

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -2,7 +2,7 @@ import img from '../IMG_6636.jpg'
 
 export const About = () => {
     return (
-        <div className="bg-whiter text-black flex flex-col h-screen justify-center items-center gap-8">
+        <div id="About" className="bg-whiter text-black flex flex-col h-screen justify-center items-center gap-8">
             <div className="header font-face-bsr text-4xl underline decoration-inherit decoration-2 underline-offset-4">About</div>
             <div className="main-section flex gap-10 justify-center">
                 <div className="container font-face-bsr text-xl w-1/5">
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -3,12 +3,12 @@
 export const NavBar = () => {
     return (
         <div className='navbar flex justify-between mx-10 font-face-bsr text-lg content-center py-5'>
-            <div className='font-semibold hover:text-orange cursor-pointer'>JGC</div>
+            <a href="#" className='font-semibold hover:text-orange cursor-pointer'>JGC</a>
             <nav className='flex gap-8'>
-                <a className="hover:text-orange cursor-pointer">About</a>
-                <a className="hover:text-orange cursor-pointer">Work</a>
-                <a className="hover:text-orange cursor-pointer">Projects</a>
-                <a className="hover:text-orange cursor-pointer">Contact</a>           
+                <a href="#About"className="hover:text-orange cursor-pointer">About</a>
+                <a href="#"className="hover:text-orange cursor-pointer">Work</a>
+                <a href="#"className="hover:text-orange cursor-pointer">Projects</a>
+                <a href="#"className="hover:text-orange cursor-pointer">Contact</a>           
             </nav>
         </div>
     )
