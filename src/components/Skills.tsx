7780e92f3fbf2f84edb6d024cda449a100aff3f9
Joshua-Gabriel Camacho <shua.camacho@gmail.com>
2023-03-13 20:10:45 -0400

    Add Skills component

---
+++ src/components/Skills.tsx
@@ -0,0 +1,11 @@
+export const Skills = () => {
+    return (
+        <div className="font-face-bsr text-black">
+            <div className="bg-grey">
+                <div>
+                    hello
+                </div>
+            </div>
+        </div>
+    )
+}
