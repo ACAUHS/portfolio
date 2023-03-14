dbf966157f6171fdfe7a005ce08038d4927b1661
Joshua-Gabriel Camacho <shua.camacho@gmail.com>
2023-03-13 20:31:10 -0400

    Add hover effects to Navbar
    Removed 'Skills' section from Navbar.

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -3,13 +3,12 @@
 export const NavBar = () => {
     return (
         <div className='navbar flex justify-between mx-10 font-face-bsr text-lg content-center py-5'>
-            <div className='font-semibold'>JGC</div>
+            <div className='font-semibold hover:text-orange cursor-pointer'>JGC</div>
             <nav className='flex gap-8'>
-                <a>About</a>
-                <a>Skills</a>
-                <a>Work</a>
-                <a>Projects</a>
-                <a>Contact</a>           
+                <a className="hover:text-orange cursor-pointer">About</a>
+                <a className="hover:text-orange cursor-pointer">Work</a>
+                <a className="hover:text-orange cursor-pointer">Projects</a>
+                <a className="hover:text-orange cursor-pointer">Contact</a>           
             </nav>
         </div>
     )
